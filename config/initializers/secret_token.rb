9bd026e923568cc9ebdefc537a370e7cbfcac20f
Ryan Marfone <ryanmarfone@gmail.com>
2014-02-21 07:52:13 -0600

    new social network

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Treebook::Application.config.secret_key_base = '5097eadfee9d63e4628358255ec0c1c9f5d03c54404183619991fa0d377ee803b6773bd461b8ccc84c67857b6ea8b5b7a3848b91bf0fa7c5d549236581fbd032'
